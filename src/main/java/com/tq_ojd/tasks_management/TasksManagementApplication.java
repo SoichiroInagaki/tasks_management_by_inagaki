ccad547c64479efc49beb6105eb45cae12453843
5593501507 <soichiro.inagaki@excite-software.co.jp>
2024-07-09 17:05:00 +0900

    docs: Mybatis generator によるO/Rマッピングファイル作成

--- src/main/java/com/tq_ojd/tasks_management/TasksManagementApplication.java
+++ src/main/java/com/tq_ojd/tasks_management/TasksManagementApplication.java
@@ -1,9 +1,11 @@
 package com.tq_ojd.tasks_management;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.tq_ojd.tasks_management.infra.mapper")
 public class TasksManagementApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/tq_ojd/tasks_management/domain/TasksManagementService.java
@@ -0,0 +1,8 @@
+package com.tq_ojd.tasks_management.domain;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TasksManagementService {
+  
+}
